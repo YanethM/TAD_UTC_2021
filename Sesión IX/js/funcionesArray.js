1288ea34b7c3d43fdce39217797c3b90e6233eb6
YanethM <YanethM@users.noreply.github.com>
2021-03-13 13:16:02 -0500

    Add files via upload

---
+++ Sesión IX/js/funcionesArray.js
@@ -0,0 +1,29 @@
+/* Generar un objeto */
+const person = {
+    /* clave: valor */
+    firstname: "Yaneth",
+    lastname: "Mejia",
+    age: 30,
+    eyeColor: "brown"
+};
+
+/* Métodos para trabajar con objetos */
+const personOne = {
+    /* clave: valor =>propiedades */
+    firstname: "Yaneth",
+    lastname: "Mejia",
+    age: 30,
+    eyeColor: "brown",
+    fullname: function() {
+        return this.firstname + " " + this.lastname;
+    }
+};
+
+/* Exiten 2 formas para acceder a las propiedades de un objeto 
+    nombreObjeto.propertyName;
+    nombreObjeto["propertyName"]
+*/
+console.log("El color de mis ojos es " + person.eyeColor);
+console.log("Tengo " + person.age + " años");
+console.log("Me llamo " + persona["firstname"]);
+console.log("Mi apellido es " + persona["lastname"]);
